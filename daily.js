5770d3d73bf8f5dd52828c53f3b9070406182972
TJ Checketts <tylerchecketts@gmail.com>
2019-04-21 16:26:59 -0600

    sunday's daily challenge

--- daily.js
+++ daily.js
@@ -1,3 +1,32 @@
+// Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
+
+// For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].
+
+// Follow-up: what if you can't use division?
+
+let arrayTimeser = (arr) => {
+  let newArray = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    let filteredNums = arr.slice(0, i).concat(arr.slice(i + 1, arr.length));
+
+    function getSum(total, num) {
+      return total * num;
+    }
+
+    newArray.push(filteredNums.reduce(getSum));
+
+    if (newArray.length === arr.length) {
+      return newArray;
+    }
+  }
+}
+
+arrayTimeser([1, 2, 8, 4]);
+
+
+
+
 // DONE
 // Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
 // For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
