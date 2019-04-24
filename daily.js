18f0b34be8421534a765ed7777382123e7584f66
TJ Checketts <tylerchecketts@gmail.com>
2019-04-23 20:49:12 -0600

    finished tuesday's challenge

--- daily.js
+++ daily.js
@@ -1,7 +1,30 @@
+// DONE
+// Given an array of integers, find the first missing positive integer in linear time and constant space. In other words, find the lowest positive integer that does not exist in the array. The array can contain duplicates and negative numbers as well.
+// For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3.
+// You can modify the input array in-place.
+
+let minPosNum = (arr) => {
+  if (Math.max(...arr) < 0) {
+    return 'At least one number has to be >= 0';
+  }
+
+  arr.sort(function(a, b){return a-b});
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] + 1 > 0 && !arr.includes(arr[i] + 1)) {
+      return arr[i] + 1;
+    }
+  }
+}
+
+minPosNum([2, -1, 5, 0, 1, 4]);
+
+
+
+
+// DONE
 // Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
-
 // For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].
-
 // Follow-up: what if you can't use division?
 
 let arrayTimeser = (arr) => {
@@ -47,64 +70,52 @@ sumBool(givenList, givenK);
 
 
 
-// number of steps, x. array of total options
-// let stepPermutations = [];
-// let stepFigureOuter = (totalSteps) => {
-//   if (totalSteps === 1) {
-//     return 1;
-//   }
-// }
-
-
-
-
 // DONE
 // Find the minimum number of coins required to make n cents.
 // You can use standard American denominations, that is, 1¢, 5¢, 10¢, and 25¢.
 // For example, given n = 16, return 3 since we can make it with a 10¢, a 5¢, and a 1¢
 
 // if remainder of 25 is 0, add up quarters and subtract from total, continue with each coin amount, return coin counts when total cents remaining is 0
+let quarterCount = 0;
+let dimeCount = 0;
+let nickelCount = 0;
+let pennyCount = 0;
 
-// let quarterCount = 0;
-// let dimeCount = 0;
-// let nickelCount = 0;
-// let pennyCount = 0;
+let minCoinCounter = (cents) => {
+  // if no cents passed or is not number, return not a cent amount
+  if (typeof cents !== 'number' || cents < 0) {
+    console.log('you must provide a positive integer please :)');
+    return;
+  }
 
-// let minCoinCounter = (cents) => {
-//   // if no cents passed or is not number, return not a cent amount
-//   if (typeof cents !== 'number' || cents < 0) {
-//     console.log('you must provide a positive integer please :)');
-//     return;
-//   }
+  // when 0 cents remaining, then return total coin counts
+  if (cents === 0) {
+    let totalCoins = quarterCount + dimeCount + nickelCount + pennyCount;
+    console.log('quarters::: ', quarterCount);
+    console.log('dimes::: ', dimeCount);
+    console.log('nickels::: ', nickelCount);
+    console.log('pennies::: ', pennyCount);
+    console.log('TOTAL COINS::: ', totalCoins);
+    return totalCoins;
+  }
 
-//   // when 0 cents remaining, then return total coin counts
-//   if (cents === 0) {
-//     let totalCoins = quarterCount + dimeCount + nickelCount + pennyCount;
-//     console.log('quarters::: ', quarterCount);
-//     console.log('dimes::: ', dimeCount);
-//     console.log('nickels::: ', nickelCount);
-//     console.log('pennies::: ', pennyCount);
-//     console.log('TOTAL COINS::: ', totalCoins);
-//     return totalCoins;
-//   }
+  // if 25 or greater, add to quarters count and decrease cents
+  // repreat for each coin amount
+  if (cents >= 25) {
+    quarterCount++;
+    cents -= 25;
+  } else if (cents >= 10) {
+    dimeCount++;
+    cents -= 10;
+  } else if (cents >= 5) {
+    nickelCount++;
+    cents -= 5;
+  } else {
+    pennyCount++;
+    cents -= 1;
+  }
 
-//   // if 25 or greater, add to quarters count and decrease cents
-//   // repreat for each coin amount
-//   if (cents >= 25) {
-//     quarterCount++;
-//     cents -= 25;
-//   } else if (cents >= 10) {
-//     dimeCount++;
-//     cents -= 10;
-//   } else if (cents >= 5) {
-//     nickelCount++;
-//     cents -= 5;
-//   } else {
-//     pennyCount++;
-//     cents -= 1;
-//   }
+  return minCoinCounter(cents);
+}
 
-//   return minCoinCounter(cents);
-// }
-
-// minCoinCounter(74);
+minCoinCounter(74);
